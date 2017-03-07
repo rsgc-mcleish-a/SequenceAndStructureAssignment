8e6f646d1daef0b21c91b91be44bafa1e46b1ed8
Student <student@mac-book-angus-mcleish.local>
2017-03-07 11:14:30 -0500

    more stuff

--- SequenceAndStructureAssignment/Assignment.playground/Pages/Design Three.xcplaygroundpage/Contents.swift
+++ SequenceAndStructureAssignment/Assignment.playground/Pages/Design Three.xcplaygroundpage/Contents.swift
@@ -22,8 +22,10 @@ import PlaygroundSupport
 let canvas = Canvas(width: 500, height: 500)
 
 // Generate a grid
-canvas.drawShapesWithFill = false
+canvas.drawShapesWithFill = true
 canvas.defaultBorderWidth = 1
+canvas.fillColor = Color.blue
+canvas.drawShapesWithBorders = false
 
 // This loop makes a 10 rows of columns
 for x in stride(from: 25, through: 475, by: 50){
@@ -32,10 +34,42 @@ for x in stride(from: 25, through: 475, by: 50){
     for y in stride(from: 25, through: 475, by: 50) {
         
         // Draw the shapes
-        canvas.drawEllipse(centreX: x, centreY: y, width: 2, height: 2)
-        canvas.drawRectangle(centreX: x, centreY: y, width: 50, height: 50)
+    canvas.drawRectangle(centreX: x, centreY: y, width: 50, height: 50)
     }
 }
+
+for x in stride(from: 25, through: 475, by: 21){
+    
+    // This loop makes a single column, bottom to top
+    for y in stride(from: 25, through: 475, by: 14) {
+        
+        // Get some entropy (a random number, 0 or 1, but never 2)
+        let value = random(from: 0, toButNotIncluding: 4)
+        
+        // Use the random value to decide on the direction of a line
+        
+        if value == 0 {
+            canvas.lineColor = Color.red
+            // Draw line that slants up and to the right
+            canvas.drawLine(fromX: x - 23, fromY: y - 30, toX: x + 21, toY: y + 25)
+            
+            //Color
+        canvas.lineColor = Color.red
+            
+        }; if value == 1 {
+            canvas.lineColor = Color.orange
+            
+            // Draw line that slants up and to the right
+            canvas.drawLine(fromX: x + 25, fromY: y - 25, toX: x - 25, toY: y + 25)
+            
+        }; if value == 2 {
+            canvas.lineColor = Color.green
+            canvas.drawLine(fromX: x - 20, fromY: y - 10, toX: x + 27, toY: y + 25)
+        }
+        
+    }
+}
+
 
 
 /*:
